8352e204304655d6707aaac45c24db73273aa1cb
boycanss <boy.simanjuntak49@gmail.com>
2022-11-18 22:46:48 +0700

    - Added blocks and ball

---
+++ Assets/Script/BallHandler.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallHandler : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D currentBallRigidbody;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint;
+    [SerializeField] private float detachDelay;
+
+    private Camera mainCamera;
+    private bool isDragging;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentBallRigidbody == null) { return; }
+
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            if(isDragging)
+            {
+                LaunchBall();
+            }
+            isDragging= false;
+            return;
+        }
+
+        isDragging = true;
+        currentBallRigidbody.isKinematic = true;
+
+        Vector2 touchPos = Touchscreen.current.primaryTouch.position.ReadValue();
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(touchPos);
+
+        currentBallRigidbody.position = worldPos;
+       
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidbody.isKinematic = false;
+        currentBallRigidbody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+    }
+
+}
